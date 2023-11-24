8b91140377059b93a5a2c64df006387e68c5cc6d
Devansh Chechani <chechanidevansh@gmail.com>
2023-11-24 19:38:26 +0530

    d

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import ExcelUploader from './ExcelUploader';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <ExcelUploader/>
+  // <Router>
+  //     <Routes>
+  //       <Route path="/" element={<FileUpload />} />
+  //       <Route path="/graphical-analysis" element={<GraphicalAnalysis />} />
+  //     </Routes>
+  //   </Router>
   );
 }
 
